ca56316d48e990687197885a08326becbe5731da
hayesch19 <chrishayespro@gmail.com>
2020-04-03 10:05:13 -0400

    Added Header CSS Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className="header-section">
       <h1>Vacation Picker</h1>
       <h3>For When You Can't Decide</h3>
     </div>
