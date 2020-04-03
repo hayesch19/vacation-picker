dc60f89e480b1ea3ca3561122b2a8044736bf3c9
hayesch19 <chrishayespro@gmail.com>
2020-04-03 13:36:57 -0400

    API Calls Console Log

--- src/components/SearchArea.jsx
+++ src/components/SearchArea.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 
 const SearchArea = () => {
+  const [searchParks, setSearchParks] = useState([])
+
+  const fetchParkData = async () => {
+    const resp = await Axios.get(
+      'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=Rej2n2IORGYVcDo8mXeH0eyo4Nak5GPpTmBqvMjA'
+    )
+    setSearchParks(resp.data)
+    console.log(resp.data)
+  }
+
+  useEffect(() => {
+    fetchParkData()
+  })
+
   return (
-    <div>
-      <button>Where To?</button>
+    <div className="button-area">
+      <button onClick={fetchParkData} className="search-button">
+        Where To?
+      </button>
     </div>
   )
 }
